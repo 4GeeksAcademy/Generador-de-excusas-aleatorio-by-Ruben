eb5684fca2caf82a1ecb80c2d8fb4d299b79e604
Rubeen.Frr8 <rubenferrermascarell@gmail.com>
2024-11-21 18:15:51 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El vehiculo", "Mi amigo", "El mecanico", "Mi padre"];
+  let pronouns = ["ha", "se ha", "esta", "roto"];
+  let action = ["perdido", "usado", "caído", "roto"];
+  let pronouns2 = ["ha", "ha visto", "el", "la"];
+  let object = ["llave", "telefono", "mando", "cartera"];
+  let when = [
+    "antes del trabajo",
+    "cuando estaba durmiendo",
+    "mientras hacía ejercicio",
+    "durante el almuerzo",
+    "mientras estaba jugando"
+  ];
+
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * pronouns.length);
+  let rdm3 = Math.floor(Math.random() * action.length);
+  let rdm4 = Math.floor(Math.random() * pronouns2.length);
+  let rdm5 = Math.floor(Math.random() * object.length);
+  let rdm6 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[rdm1] +
+    action[rdm2] +
+    when[rdm3] +
+    when[rdm4] +
+    when[rdm5] +
+    when[rdm6];
 };
